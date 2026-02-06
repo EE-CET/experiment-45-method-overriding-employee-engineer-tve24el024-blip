1e1971a138e74ba00b6f887c42fce74a83b74d12
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-06 16:13:45 +0530

    added

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -1,16 +1,18 @@
 class Employee {
-   
+    void display() {
+        System.out.println("Name of class is Employee");
+    }
 }
 
-class Engineer extends Employee {
-    // TODO: Override display() method
-    // 1. Print "Name of class is Engineer"
-    // 2. Call super.display() to print the parent's message
-}
+public class Engineer extends Employee {
 
-public class MethodOverriding {
-   
-        // TODO: Create an Engineer object
-        // TODO: Call the display() method
-    
+    void display() {
+        System.out.println("Name of class is Engineer");
+        super.display();
+    }
+
+    public static void main(String[] args) {
+        Engineer e = new Engineer();
+        e.display();
+    }
 }
